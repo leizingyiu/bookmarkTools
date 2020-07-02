d0503a65b6295dfc974a5199ffab9dcded189fd1
leizingyiu <youcanfindme@126.com>
2020-07-02 18:49:54 +0800

    Update getContent.js

--- getContent/getContent.js
+++ getContent/getContent.js
@@ -1,39 +1,41 @@
- var u = window.location.href;
- var dict = {
- 	'behance.net': "#project-canvas",
- 	'taobao.com': '#J_DivItemDesc'
- };
- var D = '';
- for (let d in dict) {
- 	D = u.indexOf(d) != -1 ? d : D
- };
- if (D != "") {
- 	var p = document.querySelector(dict[D]);
- 	var b = document.getElementsByTagName("body")[0];
- 	var h = document.getElementsByTagName("html")[0];
- 	h.removeChild(b);
- 	var pr = document.createElement("pre");
- 	pr.innerText = b.innerHTML;
- 	pr.style.display = "none";
- 	var bo = document.createElement("body");
- 	h.appendChild(bo);
- 	bo.appendChild(p);
- 	bo.appendChild(pr);
+javascript:
+var u = window.location.href;
+var dict = {
+	'behance.net': "#project-canvas",
+	'taobao.com': '#J_DivItemDesc'
+};
+var D = '';
+for (let d in dict) {
+	D = u.indexOf(d) != -1 ? d : D
+};
+if (D != "") {
+	var p = document.querySelector(dict[D]);
+	if(p!=null){
+	var b = document.getElementsByTagName("body")[0];
+	var h = document.getElementsByTagName("html")[0];
+	h.removeChild(b);
+	var pr = document.createElement("pre");
+	pr.innerText = b.innerHTML;
+	pr.style.display = "none";
+	var bo = document.createElement("body");
+	h.appendChild(bo);
+	bo.appendChild(p);
+	bo.appendChild(pr);
 
- 	function recovery(pr, bo) {
- 		var b = document.createElement("body");
- 		b.innerHTML = pr.innerText;
- 		var h = document.getElementsByTagName("html")[0];
- 		h.removeChild(h.getElementsByTagName('body')[0]);
- 		h.appendChild(b)
- 	}
- 	document.onkeydown = function(event) {
- 		var e = event || window.e;
- 		var keyCode = e.keyCode || e.which;
- 		switch (keyCode) {
- 		case 27:
- 			recovery(pr, bo);
- 			break
- 		}
- 	}
- }
+	function recovery(pr, bo) {
+		var b = document.createElement("body");
+		b.innerHTML = pr.innerText;
+		var h = document.getElementsByTagName("html")[0];
+		h.removeChild(h.getElementsByTagName('body')[0]);
+		h.appendChild(b)
+	}
+	document.onkeydown = function(event) {
+		var e = event || window.e;
+		var keyCode = e.keyCode || e.which;
+		switch (keyCode) {
+		case 27:
+			recovery(pr, bo);
+			break
+		}
+	}}
+}

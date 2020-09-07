3e8a708c79e807062b15acc140e520871a606694
leizingyiu <youcanfindme@126.com>
2020-09-07 23:19:05 +0800

    update

--- tbPrice2List/tbPrice2List.js
+++ tbPrice2List/tbPrice2List.js
@@ -1,79 +1,70 @@
-javascript: (function () {
-    var priceBox = document.querySelector("#J_StrPriceModBox .tb-rmb-num , #J_StrPriceModBox .tm-price");
-
-    var uls = document.querySelectorAll(".J_TSaleProp ");
-
-    var form = {};
-
-    for (let i = 0; i < uls.length; i++) {
-        form[uls[i].getAttribute('data-property')] = {};
-        lis = uls[i].getElementsByTagName('li');
-        for (let j = 0; j < lis.length; j++) {
-            form[uls[i].getAttribute('data-property')][lis[j].getElementsByTagName('span')[0].innerHTML] = {};
-            form[uls[i].getAttribute('data-property')][lis[j].getElementsByTagName('span')[0].innerHTML]['btn'] = lis[j].getElementsByTagName('a')[0];
-        }
-    }
-
-    function getObjChains() {
-        var a = arguments;
-        var obj = a[0];
-        if (a.length > 1) {
-            var n = a[1];
-        } else {
-            var n = Object.keys(obj).length - 1;
-        }
-
-        if (a.length > 1 && n == 0) {
-            result = {};
-            for (let i in obj[Object.keys(obj)[n]]) {
-                if (obj[Object.keys(obj)[n]][i]['btn'].getAttribute('aria-disabled') != "true") {
-                    obj[Object.keys(obj)[n]][i]['btn'].click();
-                    result[i] = document.querySelector("#J_StrPriceModBox .tb-rmb-num , #J_StrPriceModBox .tm-price").innerText;
-                    obj[Object.keys(obj)[n]][i]['btn'].click();
-
-                } else {
-                    result[i] = '--'
-                }
-            }
-            return result;
-        } else {
-            var result = {};
-            for (let i in obj[Object.keys(obj)[n]]) {
-                console.log('i: ' + i);
-
-                obj[Object.keys(obj)[n]][i]['btn'].click();
-                var inner = getObjChains(obj, n - 1);
-                for (let j in inner) {
-                    result[i + ':' + j] = inner[j];
-                }
-                obj[Object.keys(obj)[n]][i]['btn'].click();
-
-            }
-            console.log('n: ' + n);
-            console.log(result);
-            return result;
-        }
-    }
-    var daList = getObjChains(form);
-
-    console.log(daList);
-
-    var p = document.createElement("p");
-
-    var reg = new RegExp('{"', "g");
-    var reg1 = new RegExp('"}', "g");
-
-    var reg2 = new RegExp('"*[^g]:"*', "g");
-    var reg3 = new RegExp('","', "g");
-
-    var str = JSON.stringify(daList);
-
-    str = str.replace(reg, "<table><tr><td style='padding: 0.5em;'>");
-
-    str = str.replace(reg2, '</td><td style="padding: 0.5em;">');
-    str = str.replace(reg3, '</td></tr><td style="padding: 0.5em;">');
-
-    str = str.replace(reg1, '</td></tr></table>');
-    p.innerHTML = str;
-    document.querySelector('.tb-amount').parentElement.insertBefore(p, document.querySelector('.tb-amount'));
+javascript: (function() {
+	var priceBox = document.querySelector("#J_StrPriceModBox .tb-rmb-num , #J_StrPriceModBox .tm-price");
+	var uls = document.querySelectorAll(".J_TSaleProp ");
+	var form = {};
+	for (let i = 0; i < uls.length; i++) {
+		form[uls[i].getAttribute('data-property')] = {};
+		lis = uls[i].getElementsByTagName('li');
+		for (let j = 0; j < lis.length; j++) {
+			form[uls[i].getAttribute('data-property')][lis[j].getElementsByTagName('span')[0].innerHTML] = {};
+			form[uls[i].getAttribute('data-property')][lis[j].getElementsByTagName('span')[0].innerHTML]['btn'] = lis[j].getElementsByTagName('a')[0];
+		}
+	}
+	function getObjChains() {
+		var a = arguments;
+		var obj = a[0];
+		if (a.length > 1) {
+			var n = a[1];
+		} else {
+			var n = Object.keys(obj).length - 1;
+		}
+		if (a.length > 1 && n == 0) {
+			result = {};
+			for (let i in obj[Object.keys(obj)[n]]) {
+				if (obj[Object.keys(obj)[n]][i]['btn'].getAttribute('aria-disabled') != "true") {
+					obj[Object.keys(obj)[n]][i]['btn'].click();
+					result[i] = document.querySelector("#J_StrPriceModBox .tb-rmb-num , #J_StrPriceModBox .tm-price").innerText;
+					obj[Object.keys(obj)[n]][i]['btn'].click();
+				} else {
+					result[i] = '--'
+				}
+			}
+			return result;
+		} else {
+			var result = {};
+			for (let i in obj[Object.keys(obj)[n]]) {
+				console.log('i: ' + i);
+				obj[Object.keys(obj)[n]][i]['btn'].click();
+				var inner = getObjChains(obj, n - 1);
+				for (let j in inner) {
+					result[i + ':' + j] = inner[j];
+				}
+				obj[Object.keys(obj)[n]][i]['btn'].click();
+			}
+			console.log('n: ' + n);
+			console.log(result);
+			return result;
+		}
+	}
+	var daList = getObjChains(form);
+	console.log(daList);
+	var p = document.createElement("p");
+	var reg = new RegExp('{"', "g");
+	var reg1 = new RegExp('"}', "g");
+	var reg2 = new RegExp('"*[^g]:"*', "g");
+	var reg3 = new RegExp('","', "g");
+	var str = JSON.stringify(daList);
+	str = str.replace(reg, "<table><tr><td style='padding: 0.5em;'>");
+	str = str.replace(reg2, '</td><td style="padding: 0.5em;">');
+	str = str.replace(reg3, '</td></tr><td style="padding: 0.5em;">');
+	str = str.replace(reg1, '</td></tr></table>');
+	p.innerHTML = str;
+    p.id='yiu_tbPriceList';
+/*    p.style.whiteSpace='nowrap';*/
+    var s=document.createElement('style');
+    s.innerText='#yiu_tbPriceList{white-space: nowrap;    position: absolute;    left: 0;    top: 0;    z-index: 999;    width: 340px;    background: #fdfdfd;    padding: 1em;} #yiu_tbPriceList tr:hover{    background: #fff4db;}'
+    
+document.querySelector('.tb-sidebar,#ald-skuRight').style.position= 'relative';
+document.querySelector('.tb-sidebar,#ald-skuRight').insertBefore(p, document.querySelector('.tb-sidebar,#ald-skuRight').children[0]);
+document.querySelector('.tb-sidebar,#ald-skuRight').insertBefore(s, document.querySelector('.tb-sidebar,#ald-skuRight').children[0]);
 })()

0b5787b187b05084494df15532fb890e12b5ac8f
leizingyiu <youcanfindme@126.com>
2020-09-29 00:34:35 +0800

    fix html bug

--- getImg/getImg.js
+++ getImg/getImg.js
@@ -1,3 +1,17 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) Sun Sep 27 2020  leizingyiu
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORTOR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+
+
 javascript: /*获取图片书签by leizingyiu；*/
     (function () {
         console.log("%E8%8E%B7%E5%8F%96%E5%9B%BE%E7%89%87%E4%B9%A6%E7%AD%BEby leizingyiu @20200803-15:08:59");
